f570b0619fadf9a21e9e5d7ffb504d84c5f1b7aa
teja <tejasai977@gmail.com>
2024-04-01 18:29:35 +0530

    removed backend

--- remotebackend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "remotestorage99"
-    container_name       = "tfstate"
-    key                  = "terraformpipe.tfstate"
-    use_azuread_auth     = true
-    access_key = "83maiPlkoCCM51/kb/lF8vxIhwhlmJ2XKGgLeBaqysVKxpyUParCZ3hSFlTkWppK27EiNKK50FtJ+AStxHYNdA=="
-  }
-}
